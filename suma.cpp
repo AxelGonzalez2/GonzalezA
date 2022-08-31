586ac2274e3e997a297f016752819f82658b0073
Axel Gonzalez <axel.gonzalez@utelvt.edu.ec>
2022-08-31 14:12:01 -0500

    cree un prograna en c++

---
+++ suma.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+        cout<<endl<<endl;
+        cout<<"HOLA MUNDO SOY Axel";
+        cout<<endl<<endl;
+        return (0);
+#include <iostream>
+using namespace std;
+int main ()
+{
